369a6dc3d87fd24ba98e2d6a6db3185267de403f
aaaXorn <lcrs.rp@gmail.com>
2021-04-13 12:01:14 -0300

    casas, pontes, arvores e lagrimas

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+	[SerializeField]
+	Transform playerTransf;
+	
+    private void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag == "Water")
+		{
+			if(playerTransf.position.x > 271)
+			{
+				playerTransf.position = new Vector3 (380, 6, 260);
+			}
+			else
+			{
+				playerTransf.position = new Vector3 (216, 8, 186);
+			}
+		}
+	}
+}
