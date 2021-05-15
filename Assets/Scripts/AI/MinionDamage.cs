9b965b12e90f8b5b6a49213403315168182337e0
aaaXorn <lcrs.rp@gmail.com>
2021-05-15 20:43:49 -0300

    ataques dos minions prontos

---
+++ Assets/Scripts/AI/MinionDamage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionDamage : MonoBehaviour
+{
+	public int hp;
+	public MinionMove MM;
+	
+    private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.CompareTag("PlayerProjectile"))
+		{
+			hp--;
+		}
+		if(hp <= 0 || other.gameObject.tag == "Water")
+		{
+			MM.currentState = MinionMove.MinionState.Dying;
+		}
+		
+		if(MM.jumping && other.gameObject.tag == "Ground")
+		{
+			//faz o minion nao se teleportar de volta pro comeco do pulo
+			MM.agent.nextPosition = transform.position;
+			
+			//reativa o agent
+			MM.agent.updatePosition = true;
+			MM.agent.updateRotation = true;
+			
+			//desativa o movimento do rigid
+			MM.rigid.isKinematic = true;
+			
+			//encerra o pulo no IAWalk
+			MM.stopJump = true;
+		}
+	}
+}
---
+++ Assets/Scripts/AI/MinionMove.cs
@@ -0,0 +1,312 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionMove : MonoBehaviour
+{
+	public UnityEngine.AI.NavMeshAgent agent;
+	public GameObject target;
+	public Animator anim;
+	public Rigidbody rigid;
+	
+	public Vector3 patrolposition;
+	public float stoppedTime;
+    public float patrolDistance;//=10;
+    public float timetowait;// = 3;
+    public float aggroDistance, disengageDistance;// = 10;
+    public float attackDistance, atkStopDistance;// = 3;
+	
+	public enum MinionState
+	{
+		Stopped,
+		Patrol,
+		Dying,
+		Aggro,
+		
+		Attack,
+		Jump,
+		Shoot,
+		Flying,
+	}
+	public MinionState currentState;
+	
+	public GameObject Drop;
+	public bool dead;
+	public float deathTimer;
+	
+	public string minionType;//jumper, shooter (base e homing), flying (voa na direcao do player), shadow (aleatorio)
+	public float baseSpeed, aggroSpeed;
+	
+	public bool jumping, stopJump;
+	public Vector3 JumpForce;
+	public float jumpTimer, maxJT;
+	public float atkCD, maxAtkCD;
+	public float atkSpawnX, atkSpawnY, atkSpawnZ;
+	public GameObject Projectile, BasicShot, HomingShot;
+	
+	public bool randomize;
+	public int randomizeType;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        patrolposition = new Vector3(transform.position.x + Random.Range(-patrolDistance, patrolDistance), transform.position.y, transform.position.z + Random.Range(-patrolDistance, patrolDistance));
+		
+		if(randomize)
+		{
+			switch (randomizeType = Random.Range(0, 4))
+			{
+				case 0:
+				minionType = "Jumper";
+				break;
+				
+				case 1:
+				minionType = "ShooterB";
+				break;
+				
+				case 2:
+				minionType = "ShooterH";
+				break;
+				
+				case 3:
+				minionType = "Flier";
+				break;
+			}
+		}
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch(currentState)
+		{
+			case MinionState.Stopped:
+			Stopped();
+			break;
+			
+			case MinionState.Patrol:
+			Patrol();
+			break;
+			
+			case MinionState.Dying:
+			Dying();
+			break;
+			
+			case MinionState.Aggro:
+			Aggro();
+			break;
+			
+			case MinionState.Attack:
+			Attack();
+			break;
+			
+			case MinionState.Jump:
+			Jump();
+			break;
+			
+			case MinionState.Shoot:
+			Shoot();
+			break;
+			
+			case MinionState.Flying:
+			Flying();
+			break;
+		}
+    }
+	
+	void Stopped()
+	{
+		agent.isStopped = true;
+        //anim.SetBool("Attack", false);
+
+        if (target && Vector3.Distance(transform.position, target.transform.position) > aggroDistance)
+        {
+            currentState = MinionState.Patrol;
+        }
+		
+	}
+	
+	void Patrol()
+	{
+		agent.isStopped = false;
+        agent.SetDestination(patrolposition);
+        //anim.SetBool("Attack", false);
+        //tempo parado
+        if (agent.velocity.magnitude < 0.1f)
+        {
+            stoppedTime += Time.deltaTime;
+        }
+        //se for mais q timetowait segundos
+        if (stoppedTime> timetowait)
+        {
+            stoppedTime = 0;
+            patrolposition = new Vector3(transform.position.x + Random.Range(-patrolDistance, patrolDistance), transform.position.y, transform.position.z + Random.Range(-patrolDistance, patrolDistance));
+        }
+        //ditancia do jogador for menor q distancetotrigger
+        if (Vector3.Distance(transform.position, target.transform.position) < aggroDistance)
+        {
+			agent.speed = aggroSpeed;
+			currentState = MinionState.Aggro;
+        }
+	}
+	
+	void Aggro()
+	{
+		agent.isStopped = false;
+		agent.SetDestination(target.transform.position);
+		//anim.SetBool("Attack", false);
+		
+		//se a distancia dele for  menor q 3 ele ataca
+        if (Vector3.Distance(transform.position, target.transform.position) < attackDistance)
+        {
+            currentState = MinionState.Attack;
+        }
+
+        //se a distancia dele for  maior q o trigger ele patrulha de novo 
+        if (Vector3.Distance(transform.position, target.transform.position) > disengageDistance)
+        {
+			agent.speed = baseSpeed;
+            currentState = MinionState.Patrol;
+        }
+	}
+	
+	void Attack()
+	{
+		if (Vector3.Distance(transform.position, target.transform.position) > atkStopDistance)
+		{
+			currentState = MinionState.Aggro;
+		}
+		
+		switch(minionType)
+		{
+			case "Jumper":
+			currentState = MinionState.Jump;
+			break;
+			
+			case "ShooterB":
+			if(!Projectile) Projectile = BasicShot;
+			currentState = MinionState.Shoot;
+			break;
+			
+			case "ShooterH":
+			if(!Projectile) Projectile = HomingShot;
+			currentState = MinionState.Shoot;
+			break;
+			
+			case "Flier":
+			currentState = MinionState.Flying;
+			break;
+			
+			default:
+			break;
+		}
+	}
+	
+	void Jump()
+	{
+		Rotate();
+		
+		if(!jumping)
+		{
+			//desativa o agent
+			agent.isStopped = true;
+			agent.updatePosition = false;
+			agent.updateRotation = false;
+		
+			//ativa o movimento do rigid e faz o minion pular
+			rigid.isKinematic = false;
+			rigid.AddRelativeForce(JumpForce, ForceMode.Impulse);
+			
+			//pra evitar bugs
+			jumpTimer -= Time.deltaTime;
+			if(jumpTimer < 0)
+			{
+				jumpTimer = maxJT;
+				jumping = true;
+			}
+		}
+		
+		if(stopJump)//acontece quando a aranha toca no chao pelo AIJumpCollision
+		{
+			jumping = false;
+			stopJump = false;
+			currentState = MinionState.Aggro;
+		}
+	}
+	
+	void Shoot()
+	{
+		agent.SetDestination(target.transform.position);
+		
+		if(atkCD > 0)
+			atkCD -= Time.deltaTime;
+		else
+		{
+			GameObject Attack = Instantiate(Projectile,
+											transform.position,
+											transform.rotation);
+			Vector3 spawnPoint = transform.TransformDirection(atkSpawnX, atkSpawnY, atkSpawnZ);
+			Attack.transform.position = Attack.transform.position + spawnPoint;
+			
+			atkCD = maxAtkCD;
+			currentState = MinionState.Aggro;
+		}
+	}
+	
+	void Flying()
+	{
+		if(!jumping)
+		{
+			//desativa o agent
+			agent.isStopped = true;
+			agent.updatePosition = false;
+			agent.updateRotation = false;
+			//ativa o movimento do rigid
+			rigid.isKinematic = false;
+			
+			jumping = true;
+		}
+		
+		//faz o minion voar
+		transform.LookAt(target.transform.position + new Vector3(0, 1, 0));
+		rigid.velocity = transform.forward * aggroSpeed;
+		
+		if (Vector3.Distance(transform.position, target.transform.position) > disengageDistance)
+        {
+			jumping = false;
+			
+			//faz o minion nao se teleportar de volta pro comeco do pulo
+			agent.nextPosition = transform.position;
+			
+			//reativa o agent
+			agent.updatePosition = true;
+			agent.updateRotation = true;
+			
+			//desativa o movimento do rigid
+			rigid.isKinematic = true;
+			
+			agent.speed = baseSpeed;
+            currentState = MinionState.Patrol;
+        }
+	}
+	
+	void Dying()
+	{
+		agent.isStopped = true;
+        //anim.SetBool("Attack", false);
+		if(!dead)
+		{
+			//anim.SetTrigger("Die");
+			Instantiate(Drop, transform.position, Quaternion.identity);
+			dead = true;
+		}
+        Destroy(gameObject, deathTimer);
+	}
+	
+	void Rotate()
+	{
+		Vector3 direction = (target.transform.position - transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 6);
+	}
+}
